c3c832fcfdcce5867eacea38e9850e3ddd3a1292
Saeed Shoraka <saeedshoraka89@gmail.com>
2023-06-05 17:17:52 +0330

    First Update

--- Framework/GlobalUsings.cs
+++ Framework/GlobalUsings.cs
@@ -1,8 +1,7 @@
-// Global using directives
 
+global using Framework.Domain;
+global using Microsoft.EntityFrameworkCore;
 global using System.Globalization;
 global using System.Linq.Expressions;
 global using System.Text;
 global using System.Text.RegularExpressions;
-global using Framework.Domain;
-global using Microsoft.EntityFrameworkCore;
